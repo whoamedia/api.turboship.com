35e630c30d007798eb02bee927dc82d3a49e110c
James Weston <james@caravana.rentals>
2016-12-12 15:12:39 -0500

    Updating database configuration

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST_' . strtoupper(env('APP_ENV'))),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE_' . strtoupper(env('APP_ENV'))),
+            'username'  => env('DB_USERNAME_' . strtoupper(env('APP_ENV'))),
+            'password'  => env('DB_PASSWORD_' . strtoupper(env('APP_ENV'))),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
