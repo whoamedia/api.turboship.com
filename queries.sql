e9465a0cb89208935ae7c490b061d73cdb06baa6
James Weston <james@caravana.rentals>
2016-12-26 13:26:37 -0500

    Having ShopifyOrderController function createOrder use new queueing system

--- queries.sql
+++ queries.sql
@@ -14,7 +14,8 @@ FROM
   JOIN IntegratedShoppingCart integratedShoppingCart ON integratedShoppingCart.id = webHookLog.integratedShoppingCartId
   JOIN IntegratedService integratedService ON integratedService.id = integratedShoppingCart.id
   JOIN Client client ON client.id = integratedShoppingCart.clientId
-  JOIN Organization organization ON organization.id = client.organizationId;
+  JOIN Organization organization ON organization.id = client.organizationId
+WHERE webHookLog.topic = 'orders/create';
 
 
 select id, integratedShoppingCartId, topic, verified, success, entityId, externalId, entityCreated, notes, errorMessage, createdAt from ShopifyWebHookLog;
