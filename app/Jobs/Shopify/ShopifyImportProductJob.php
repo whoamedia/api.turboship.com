7011282e5377577beddfa1b3da262b041dd91727
James Weston <james@caravana.rentals>
2016-12-26 14:23:39 -0500

    Utilizing more queues for shopify web hooks

--- app/Jobs/Shopify/ShopifyImportProductJob.php
+++ app/Jobs/Shopify/ShopifyImportProductJob.php
@@ -4,6 +4,7 @@ namespace App\Jobs\Shopify;
 
 use App\Integrations\Shopify\Models\Responses\ShopifyProduct;
 use App\Repositories\Doctrine\CMS\ClientRepository;
+use App\Repositories\Doctrine\Logs\ShopifyWebHookLogRepository;
 use App\Repositories\Doctrine\OMS\ProductRepository;
 use App\Services\Shopify\Mapping\ShopifyProductMappingService;
 use Illuminate\Bus\Queueable;
@@ -27,6 +28,11 @@ class ShopifyImportProductJob implements ShouldQueue
     private $clientId;
 
     /**
+     * @var int|null
+     */
+    private $shopifyWebHookLogId;
+
+    /**
      * @var ClientRepository
      */
     private $clientRepo;
@@ -37,14 +43,21 @@ class ShopifyImportProductJob implements ShouldQueue
     private $productRepo;
 
     /**
+     * @var ShopifyWebHookLogRepository
+     */
+    private $shopifyWebHookLogRepo;
+
+    /**
      * ShopifyImportProductJob constructor.
      * @param   ShopifyProduct  $shopifyProduct
      * @param   int             $clientId
+     * @param   int|null        $shopifyWebHookLogId
      */
-    public function __construct($shopifyProduct, $clientId)
+    public function __construct($shopifyProduct, $clientId, $shopifyWebHookLogId = null)
     {
         $this->shopifyProduct           = $shopifyProduct;
         $this->clientId                 = $clientId;
+        $this->shopifyWebHookLogId      = $shopifyWebHookLogId;
     }
 
     /**
@@ -56,13 +69,38 @@ class ShopifyImportProductJob implements ShouldQueue
     {
         $this->clientRepo               = EntityManager::getRepository('App\Models\CMS\Client');
         $this->productRepo              = EntityManager::getRepository('App\Models\OMS\Product');
+        $this->shopifyWebHookLogRepo    = EntityManager::getRepository('App\Models\Logs\ShopifyWebHookLog');
+        $shopifyWebHookLog              = is_null($this->shopifyWebHookLogId) ? null : $this->shopifyWebHookLogRepo->getOneById($this->shopifyWebHookLogId);
         $client                         = $this->clientRepo->getOneById($this->clientId);
 
         $shopifyProductMappingService   = new ShopifyProductMappingService($client);
+
+        if (!is_null($shopifyWebHookLog))
+            $shopifyWebHookLog->setExternalId($this->shopifyProduct->getId());
+
         if (!$shopifyProductMappingService->shouldImport($this->shopifyProduct))
+        {
+            if (!is_null($shopifyWebHookLog))
+            {
+                $shopifyWebHookLog->addNote('shouldImportOrder was false');
+                $this->shopifyWebHookLogRepo->saveAndCommit($shopifyWebHookLog);
+            }
             return;
+        }
+
 
         $product                        = $shopifyProductMappingService->handleMapping($this->shopifyProduct);
+        $entityCreated                  = is_null($product->getId()) ? true : false;
+
+        if (!is_null($shopifyWebHookLog))
+            $shopifyWebHookLog->setEntityCreated($entityCreated);
+
         $this->productRepo->saveAndCommit($product);
+        if (!is_null($shopifyWebHookLog))
+        {
+            $shopifyWebHookLog->setEntityId($product->getId());
+            $this->shopifyWebHookLogRepo->saveAndCommit($shopifyWebHookLog);
+        }
+
     }
 }
