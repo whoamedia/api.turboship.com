8bc86426f9f65ab176ae7cce29c1dc24eccbf796
James Weston <james@caravana.rentals>
2016-12-28 16:24:18 -0500

    Trying to get Shopify web hooks to work again

--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -9,7 +9,14 @@ abstract class Job
 
     public function __construct()
     {
-
+        /**
+         * Connect if the connection is not available
+         */
+        if (!EntityManager::getConnection()->ping())
+        {
+            EntityManager::getConnection()->close();
+            EntityManager::getConnection()->connect();
+        }
     }
 
 }
