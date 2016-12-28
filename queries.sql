c47f03ac5fb6efb7959ad12c3f3ff345efd7b446
James Weston <james@caravana.rentals>
2016-12-28 10:56:29 -0500

    Modifying ShopifyWebHookLog query

--- queries.sql
+++ queries.sql
@@ -14,7 +14,8 @@ FROM
   JOIN IntegratedShoppingCart integratedShoppingCart ON integratedShoppingCart.id = webHookLog.integratedShoppingCartId
   JOIN IntegratedService integratedService ON integratedService.id = integratedShoppingCart.id
   JOIN Client client ON client.id = integratedShoppingCart.clientId
-  JOIN Organization organization ON organization.id = client.organizationId;
+  JOIN Organization organization ON organization.id = client.organizationId
+  WHERE webHookLog.entityId IS NULL and webHookLog.externalId IS NULL AND webHookLog.notes IS NULL and webHookLog.errorMessage IS NULL;
 
 
 select id, integratedShoppingCartId, topic, verified, success, entityId, externalId, entityCreated, notes, errorMessage, createdAt from ShopifyWebHookLog;
