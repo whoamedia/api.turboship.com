9058a265956260bb3df7e166759765ca46c2397a
James Weston <james@caravana.rentals>
2016-12-30 16:35:22 -0500

    Ensuring that phone number is always set in approval process

--- app/Services/Order/OrderApprovalService.php
+++ app/Services/Order/OrderApprovalService.php
@@ -151,16 +151,13 @@ class OrderApprovalService
         $address->setSubdivision($subdivision);
 
         /**
-         * Make sure that the phone number is set for international shipments
+         * Ensure that the phone number is always set
          */
-        if ($country->getIso2() != 'US')
+        if (is_null($address->getPhone()) || empty(trim($address->getPhone())))
         {
-            if (is_null($address->getPhone()) || empty(trim($address->getPhone())))
-            {
-                $status                     = $this->orderStatusRepo->getOneById(OrderStatusUtility::INVALID_PHONE_NUMBER);
-                $order->addStatus($status);
-                return false;
-            }
+            $status                     = $this->orderStatusRepo->getOneById(OrderStatusUtility::INVALID_PHONE_NUMBER);
+            $order->addStatus($status);
+            return false;
         }
 
         $order->setShippingAddress($address);
