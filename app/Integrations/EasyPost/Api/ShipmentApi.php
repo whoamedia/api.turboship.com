4ca980e8127d0441a3c40511dfa42c94bd329b6f
James Weston <james@caravana.rentals>
2016-12-30 18:12:24 -0500

    Debugging EasyPost API

--- app/Integrations/EasyPost/Api/ShipmentApi.php
+++ app/Integrations/EasyPost/Api/ShipmentApi.php
@@ -56,10 +56,7 @@ class ShipmentApi extends BaseApi
     public function create ($request = [])
     {
         $request                        = $request instanceof CreateEasyPostShipment ? $request : new CreateEasyPostShipment($request);
-        print_r(['shipment' => $request->jsonSerialize()]);
         $response                       = parent::makeHttpRequest('post', $this->path, ['shipment' => $request->jsonSerialize()]);
-        print_r($response);
-        die;
         return new EasyPostShipment($response);
     }
 
