acd8a995bb9f4613277bd5d958020a06ac8e96b7
James Weston <james@caravana.rentals>
2017-01-19 10:40:40 -0500

    Trying to dynamically create workers

--- .ebextensions/parseConfig.php
+++ .ebextensions/parseConfig.php
@@ -18,11 +18,7 @@ EOT;
 $programs           = '';
 $programs           .= generateProgram('beanstalkd', 'shopifyOrders', 5, 5, 2, 1);
 
-$superLocation = '';
-if(file_exists('/var/app/ondeck/.ebextensions/supervisord.conf'))
-    $superLocation = '/var/app/ondeck/.ebextensions/supervisord.conf';
-else if(file_exists('/var/app/current/.ebextensions/supervisord.conf'))
-    $superLocation = '/var/app/current/.ebextensions/supervisord.conf';
+$superLocation      = '.ebextensions/supervisord.conf';
 
 file_put_contents($superLocation, $programs.PHP_EOL, FILE_APPEND);
 
