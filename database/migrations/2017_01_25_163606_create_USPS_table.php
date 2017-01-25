005f0e2ec2a31e9785ef4b99ca33d5376561695e
James Weston <james@caravana.rentals>
2017-01-25 11:43:30 -0500

    Adding createdBy to OrderStatusHistory

---
+++ database/migrations/2017_01_25_163606_create_USPS_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUSPSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('USPS', function (Blueprint $table)
+        {
+            $table->increments('id')->unsigned();
+
+            $table->integer('orderId')->unsigned()->index();
+            $table->integer('statusId')->unsigned()->index();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('USPS');
+    }
+}
