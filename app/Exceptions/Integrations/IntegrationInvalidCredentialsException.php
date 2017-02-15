1f4a4806041617e1d5ee3e61f0d25339efaab2c2
James Weston <james@caravana.rentals>
2017-02-15 17:56:40 -0500

    Forcing message to be set in IntegrationInvalidCredentialsException constructor

--- app/Exceptions/Integrations/IntegrationInvalidCredentialsException.php
+++ app/Exceptions/Integrations/IntegrationInvalidCredentialsException.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class IntegrationInvalidCredentialsException extends HttpException
 {
 
-    public function __construct($message = null, \Exception $previous = null, array $headers = array(), $code = 0)
+    public function __construct($message, \Exception $previous = null, array $headers = array(), $code = 0)
     {
         parent::__construct(403, $message, $previous, $headers, $code);
     }
