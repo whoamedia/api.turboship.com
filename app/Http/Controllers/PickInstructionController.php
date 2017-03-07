76e8066aeabaf3b66b68e4b939b18978ce736949
James Weston <james@caravana.rentals>
2017-03-07 16:50:35 -0500

    Improving pick instruction API operations

--- app/Http/Controllers/PickInstructionController.php
+++ app/Http/Controllers/PickInstructionController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\PickInstructions\CreatePickInstruction;
+use App\Http\Requests\PickInstructions\ShowPickInstruction;
+use App\Models\WMS\PickInstruction;
+use App\Models\WMS\Validation\PickInstructionValidation;
+use App\Repositories\Doctrine\WMS\PickInstructionRepository;
 use Illuminate\Http\Request;
 use EntityManager;
 
 class PickInstructionController extends BaseAuthController
 {
 
+    /**
+     * @var PickInstructionRepository
+     */
+    private $pickInstructionRepo;
+
+    /**
+     * @var PickInstructionValidation
+     */
+    private $pickInstructionValidation;
+
+
+    public function __construct ()
+    {
+        $this->pickInstructionRepo      = EntityManager::getRepository('App\Models\WMS\PickInstruction');
+        $this->pickInstructionValidation= new PickInstructionValidation();
+    }
+    
     public function index (Request $request)
     {
 
@@ -16,12 +38,14 @@ class PickInstructionController extends BaseAuthController
 
     public function show (Request $request)
     {
-
+        $id                             = $request->route('id');
+        $pickInstruction                = $this->getPickInstructionFromRoute($id);
+        return response($pickInstruction);
     }
 
     public function store (Request $request)
     {
-
+        $createPickInstruction          = new CreatePickInstruction($request->input());
     }
 
     public function update (Request $request)
@@ -29,4 +53,18 @@ class PickInstructionController extends BaseAuthController
 
     }
 
+    /**
+     * @param   int     $id
+     * @return  PickInstruction
+     */
+    private function getPickInstructionFromRoute ($id)
+    {
+        $showPickInstruction            = new ShowPickInstruction();
+        $showPickInstruction->setId($id);
+        $showPickInstruction->validate();
+        $showPickInstruction->clean();
+
+        $pickInstruction                = $this->pickInstructionValidation->idExists($showPickInstruction->getId());
+        return $pickInstruction;
+    }
 }
--- app/Repositories/Doctrine/WMS/PickInstructionRepository.php
+++ app/Repositories/Doctrine/WMS/PickInstructionRepository.php
@@ -3,9 +3,21 @@
 namespace App\Repositories\Doctrine\WMS;
 
 
+use App\Models\WMS\PickInstruction;
 use App\Repositories\Doctrine\BaseRepository;
 
 class PickInstructionRepository extends BaseRepository
 {
+
+
+
+    /**
+     * @param   int         $id
+     * @return  PickInstruction|null
+     */
+    public function getOneById($id)
+    {
+        return $this->find($id);
+    }
 
 }
